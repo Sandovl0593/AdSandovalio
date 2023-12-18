4459e81c96ae331f54e86c06b684fbd01ad12951
Sandovl0593 <adrian.sandoval@utec.edu.pe>
2023-12-17 20:51:40 -0500

    In struccture

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,19 +1,7 @@
 /** @type {import("prettier").Config} */
 export default {
-    plugins: [
-      "prettier-plugin-svelte",
-      "prettier-plugin-astro",
-      "prettier-plugin-tailwindcss"
-    ],
-    tailwindAttributes: ['class', 'className'],
-    tailwindConfig: './tailwind.config.js',
+    plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+    tailwindAttributes: ["class", "className"],
+    tailwindConfig: "./tailwind.config.js",
     tabWidth: 4,
-    overrides: [
-      {
-        files: '*.astro',
-        options: {
-          parser: 'astro',
-        },
-      },
-    ],
-  };
+};
