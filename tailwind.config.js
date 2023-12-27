795e4dda79950f307c0feef81b302e1fef547b52
Sandovl0593 <adrian.sandoval@utec.edu.pe>
2023-12-27 15:52:25 -0500

    starting profile style

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,24 +4,8 @@ export default {
   theme: {
     extend: {
       screens: {
-        md: { max: "850px" },
-        // lmd: { max: "850px" },<
-      },
-      colors: {
-        mantis: {
-          50: "#f2fbf3",
-          100: "#e0f8e2",
-          200: "#c2f0c6",
-          300: "#93e29b",
-          400: "#5ccc69",
-          500: "#3bc44b",
-          600: "#279233",
-          700: "#22732c",
-          800: "#1f5c27",
-          900: "#1c4b23",
-          950: "#0a290f",
-          980: "#071a0a",
-        },
+        md: { max: "1100px" },
+        lmd: { max: "850px" },
       },
       plugins: [],
     },
